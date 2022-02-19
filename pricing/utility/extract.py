01f0f5152ef6d38ccf36de3ac9bc503db07e730b
Ryan Kelleher <ryank2k12@gmail.com>
2022-02-19 18:01:44 +0000

    Added S3 pricing functionality

--- pricing/utility/extract.py
+++ pricing/utility/extract.py
@@ -23,3 +23,14 @@ def extract_values(obj, key):
     results = extract(obj, arr, key)
 
     return results
+
+def item_generator(json_input, lookup_key):
+    if isinstance(json_input, dict):
+        for k, v in json_input.items():
+            if k == lookup_key:
+                yield v
+            else:
+                yield from item_generator(v, lookup_key)
+    elif isinstance(json_input, list):
+        for item in json_input:
+            yield from item_generator(item, lookup_key)
